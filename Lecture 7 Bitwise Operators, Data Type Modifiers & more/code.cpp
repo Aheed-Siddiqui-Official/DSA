12c1c3c1a9a5ddff5db7347483c566fcfc7c85db
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-03-25 06:03:08 +0500

    commit

---
+++ Lecture 7 Bitwise Operators, Data Type Modifiers & more/code.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 4, b = 8;
+
+                                        // BITWISE AND &
+    // cout << (a & b) << endl;
+
+                                        // BITWISE OR |
+    // cout << (a | b) << endl;
+
+                                        // BITWISE XOR ^
+    // cout << (a ^ b) << endl;
+
+                                        // BITWISE LEFT SHIFT <<
+    // cout << (5 << 2) << endl;
+    // cout << (10 << 2) << endl;
+
+                                        // BITWISE RIGHT SHIFT >>
+    // cout << (5 >> 2) << endl;
+    // cout << (10 >> 1) << endl;
+
+                                        // DATATYPES AND MODIFIERS
+    cout << sizeof(int) << endl;
+    cout << sizeof(long int) << endl;
+    cout << sizeof(short int) << endl;
+    cout << sizeof(long long int) << endl;
+
+    return 0;
+}
