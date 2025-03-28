8765f0b00b0270d37ee499587e77a47c389ea078
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-03-29 01:17:15 +0500

    commit

--- Lecture 9 Vectors - Arrays Part 2/code.cpp
+++ Lecture 9 Vectors - Arrays Part 2/code.cpp
@@ -46,8 +46,57 @@ int main() {
     //     cout << val << endl;
     // }
 
-   
+                                        // VECTOR FUNCTIONS
+
+    // vector<char> vec = {'a', 'b', 'c', 'd', 'e'};
+
+    //                                     // SIZE
+
+    // cout << "size = " << vec.size() << endl;
+
+    //                                     // PUSH BACK ADDS ELEMENT AFTER LAST
+
+    // vec.push_back('f');
+    // vec.push_back('g');
+    // vec.push_back('h');
+    // cout << "size after push back = " << vec.size() << endl;
+
+    //                                     // POP BACK DELETES ELEMENT FROM LAST
+
+    // vec.pop_back();
+    // cout << "size after pop back = " << vec.size() << endl;
+
+    //                                     // FRONT PRINTS FIRST VALUE
     
+    // cout << "first value = " << vec.front() << endl;
+
+    //                                     // BACK PRINTS LAST VALUE
+
+    // cout << "last value = " << vec.back() << endl;
+                                        
+    //                                     // AT INDEX NO
+
+    // cout << "target index = " << vec.at(3) << endl;
+
+                                            // MEMORY ALLOCATION IN VECTORS
+
+    // vector<int> vec;
+
+    // vec.push_back(0);
+    // vec.push_back(1);
+    // vec.push_back(2);
+    // cout << vec.size() << endl; // 3
+    // cout << vec.capacity() << endl;  // 4
+
+    // vec.push_back(0);
+    // vec.push_back(1);
+    // vec.push_back(2);
+    // vec.push_back(3);
+    // vec.push_back(4);
+    // cout << vec.size() << endl; // 5
+    // cout << vec.capacity() << endl;  // 8
+
+      
 
     return 0;
 }
