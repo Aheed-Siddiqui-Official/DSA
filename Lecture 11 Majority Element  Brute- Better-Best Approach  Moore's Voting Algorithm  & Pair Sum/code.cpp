5966117d535d6052c0dd1f8c0a86b2efe1e33751
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-04-01 07:38:50 +0500

    commit

---
+++ Lecture 11 Majority Element  Brute- Better-Best Approach  Moore's Voting Algorithm  & Pair Sum/code.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+                                        // PAIR SUM BRUTE APPROACH
+
+                        // 2,4, 6, 8, 10      14          5
+    // vector<int> pairSum(vector<int> vec, int target, int size) {
+    //     vector<int> ans;
+
+
+    //     for (int i = 0 ; i < size ; i++) {
+    //         for (int j = i + 1 ; j < size ; j++) {
+    //             if (vec[i] + vec[j] == target) {
+    //                 ans.push_back(i);
+    //                 ans.push_back(j);
+    //                 return ans;
+    //             }   
+    //         }    
+    //     }
+
+    //     return ans;
+    //}
+
+  
+
+int main() {
+
+    int size;
+    cout << "Enter size of array = ";
+    cin >> size;
+
+    vector<int> vec(size);
+
+    for (int i = 0 ; i < size ; i++) {
+        cin >> vec[i];
+    }
+
+    for (int j = 0 ; j < size ; j++) {
+        cout << vec[j] << " ";
+    }
+
+    cout << endl;
+
+    int targetSum;
+    cout << "Enter targeted sum = ";
+    cin >> targetSum;
+
+                                        // PAIR SUM BRUTE APPROACH
+
+    // vector<int> ans = pairSum(vec, targetSum, size);
+    // cout << ans[0] << ", " << ans[1];
+
+                                       
+    
+  
+    
+
+
+    return 0;
+}
