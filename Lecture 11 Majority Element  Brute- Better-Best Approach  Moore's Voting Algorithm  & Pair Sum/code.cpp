ff0e06c75dfb0ac5c1535bef8a87fa63442584f6
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-04-07 20:23:08 +0500

    commit

--- Lecture 11 Majority Element  Brute- Better-Best Approach  Moore's Voting Algorithm  & Pair Sum/code.cpp
+++ Lecture 11 Majority Element  Brute- Better-Best Approach  Moore's Voting Algorithm  & Pair Sum/code.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
 
                                         // PAIR SUM BRUTE APPROACH
@@ -24,27 +26,106 @@ using namespace std;
 
                                         // PAIR SUM OPTIMIZED APPROACH
 
-     vector<int> pairSum(vector<int> vec, int size, int target) {
-        vector<int> ans;
-            int i = 0;
-            int j = size - 1;
+    //  vector<int> pairSum(vector<int>& vec, int size, int target) {
+    //     vector<int> ans;
+    //         int i = 0;
+    //         int j = size - 1;
 
-            while (i < j) {
-                int pairSum = vec[i] + vec[j];
+    //         while (i < j) {
+    //             int pairSum = vec[i] + vec[j];
 
-                if (pairSum > target) {
-                    j--;
-                } else if (pairSum < target) {
-                    i++;
-                } else {
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    return ans;
-                }
+    //             if (pairSum > target) {
+    //                 j--;
+    //             } else if (pairSum < target) {
+    //                 i++;
+    //             } else {
+    //                 ans.push_back(i);
+    //                 ans.push_back(j);
+    //                 return ans;
+    //             }
                 
+    //         }
+    //         return ans;
+    // }
+
+
+                                        // MAJORITY ELEMENT BRUTE FORCE
+
+    // int majorityElement(vector<int>& vec, int size) {
+
+    //     for(int val : vec) {
+    //         int freq = 0;
+
+    //         for(int el : vec) {
+    //             if (el == val) {
+    //                 freq++;
+    //             }
+    //         }
+    //         if(freq > size/2) {
+    //             return val;
+    //         }
+    //     }
+    //     return -1;
+    // }
+
+                                        // MAJORITY ELEMENT OPTIMIZED
+
+    // int majorityElement(vector<int>& vec, int size) {
+    
+    //     sort(vec.begin(), vec.end());
+
+    //     int freq = 1;
+    //     int ans = vec[0];
+
+    //     for (int i = 0 ; i < size ; i++) {
+    //         if(vec[i] == vec[i - 1]) {
+    //             freq++;
+    //         } else {
+    //             freq = 1;
+    //             ans = vec[i];
+    //         }
+
+    //         if(freq > size / 2) {
+    //             return ans;
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+    int majorityElement(vector<int>& vec, int size) {
+        int freq = 0;
+        int ans;
+
+        for(int i = 0 ; i < size ; i++) {
+            if(freq == 0){
+                ans = vec[i];
+            } if (ans == vec[i]) {
+                freq++;
+            } else {
+                freq--;
             }
+        }
+
+        int count = 0;
+        for(int val : vec) {
+            if(val == ans) {
+                count++;
+            }
+        }
+
+        if(count > size / 2) {
             return ans;
+        } else {
+            return -1;
+        }
+
+
+        return ans;
     }
+
+
+
+
 
 int main() {
 
@@ -58,27 +139,48 @@ int main() {
         cin >> vec[i];
     }
 
-    for (int j = 0 ; j < size ; j++) {
-        cout << vec[j] << " ";
+    for (int i = 0 ; i < size ; i++) {
+        cout << vec[i] << " ";
     }
 
     cout << endl;
-
-    int targetSum;
-    cout << "Enter targeted sum = ";
-    cin >> targetSum;
 
                                         // PAIR SUM BRUTE APPROACH
 
     // vector<int> ans = pairSum(vec, targetSum, size);
     // cout << ans[0] << ", " << ans[1];
 
+    // int targetSum;
+    // cout << "Enter targeted sum = ";
+    // cin >> targetSum;
+
                                         // PAIR SUM OPTIMIZED APPROACH
     
-    vector<int> ans = pairSum(vec, targetSum, size);
-    cout << ans[0] << ", " << ans[1];
-    
-    
+    // vector<int> ans = pairSum(vec, targetSum, size);
+    // cout << ans[0] << ", " << ans[1];
+
+    // int targetSum;
+    // cout << "Enter targeted sum = ";
+    // cin >> targetSum;
+
+                                        // MAJORITY ELEMENT
+
+                                        //BRUTE FORCE
+   
+    // cout << "Majority Element = " << majorityElement(vec, size);
+
+                                        //OPTIMIZED
+
+    // cout << "Majority Element = " << majorityElement(vec, size);
+                                            
+                                        //MOORE'S VOTING ALGORITHM
+
+    cout << "Majority Element = " << majorityElement(vec, size);
+
+
+
+
+
 
 
     return 0;
