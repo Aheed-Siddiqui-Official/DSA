ed03ec3732bec91653496792664cd2d6b5e61ff0
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-04-18 20:27:09 +0500

    commit

---
+++ Lecture 12 Time & Space Complexity/code.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+                                        // COMMON TIME COMPLEXITIES
+    
+                                        // O(1) => CONSTANT (SUM OF NUM 1 TO N)
+    int n;
+    cin >> n;
+
+    int ans = n * (n+1)/2;
+    // cout << ans;
+
+
+    
+
+    return 0;
+}
