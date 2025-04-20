cf144a640de116e7a0efdf8e1bf3a752c5c2f482
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-04-20 06:08:26 +0500

    commit

---
+++ Lecture 14 Container with Most Water Problem  Brute & Optimal Solution  Two Pointer Approach - Leetcode 11/code.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> vec = {1, 8 , 6, 2, 5, 4, 8, 3, 7};
+
+    // int maxWater = 0;
+
+    // for (int i = 0 ; i < vec.size() ; i++) {
+    //     for (int j = i + 1 ; j < vec.size() ; j++) {
+    //         int w = j - i;
+    //         int h = min(vec[i], vec[j]);
+    //         int area = w * h;
+    //         maxWater = max(maxWater, area);
+    //     }
+    // }
+
+    
+
+
+    return 0;
+}
