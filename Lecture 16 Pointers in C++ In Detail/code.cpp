b54e104ac2aab5099205649ed211924dd7183daa
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-04-22 06:09:03 +0500

    commit

--- Lecture 16 Pointers in C++ In Detail/code.cpp
+++ Lecture 16 Pointers in C++ In Detail/code.cpp
@@ -62,6 +62,49 @@ int main() {
 
     // cout << a;
 
+                                        // ARRAY POINTER
+
+    // int arr[] = {1, 2, 3, 4, 5}; // Constant Pointer
+
+    // cout << arr << endl;
+    // cout << *arr << endl;
+
+                                        // POINTER ARITHMETIC
+
+    // INCREMENT (++), DECREMENT (--) => addition in memory according to data type 
+
+    // int a = 10;
+    // int *ptr = &a;
+
+    // cout << ptr << endl;
+    // ptr++;
+    // cout << ptr << endl;
+    // ptr--;
+    // cout << ptr << endl;
+
+    // ADD SUBTRACT => addition in memory according to data type
+    
+    // cout << ptr << endl;
+    // ptr = ptr+4;
+    // cout << ptr << endl;
+    // ptr = ptr-3;
+    // cout << ptr << endl;
+
+    // ADD SUBTRACT IN ARRAY
+
+    // int arr[] = {1, 2, 3, 4, 5};
+
+    // cout << *arr << endl;
+    // cout << *(arr+1) << endl;
+    // cout << *(arr+2) << endl;
+    
+    // ADDITION OF POINTERS
+
+    // int *ptr;
+    // int *ptr2 = ptr + 2;
+    
+    // cout << ptr2 - ptr;
+
 
     
     return 0;
