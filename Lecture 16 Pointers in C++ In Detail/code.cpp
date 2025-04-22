6b50978d1cf06b0fea16c7f90c2a8228c52d8d43
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-04-22 06:06:49 +0500

    commit

---
+++ Lecture 16 Pointers in C++ In Detail/code.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void changeA(int *a){
+    *a = 20;
+}
+
+int main() {
+
+                                        // POINTERS
+
+                                        // DEFINITION
+
+    // int a = 10;
+    // int *ptr = &a;
+
+    // cout << ptr, &a;
+
+    // float price = 100.25;
+    // float *ptr = &price;
+
+    // cout << ptr, &price;
+
+
+    
+    return 0;
+}
