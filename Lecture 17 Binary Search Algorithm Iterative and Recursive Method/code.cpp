d5dd141d10c9abeec3408c6f4668b95066611653
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-04-23 05:48:08 +0500

    commit

--- Lecture 17 Binary Search Algorithm Iterative and Recursive Method/code.cpp
+++ Lecture 17 Binary Search Algorithm Iterative and Recursive Method/code.cpp
@@ -14,7 +14,10 @@ int main() {
 
     cout << binarySearch(oddArr, oddTar) << endl;
 
-    
+    vector<int> evenArr = {-1, 0, 3, 5, 9, 12};
+    int evenTar = 0;
+
+    cout << binarySearch(evenArr, evenTar) << endl;
 
     return 0;
 }
