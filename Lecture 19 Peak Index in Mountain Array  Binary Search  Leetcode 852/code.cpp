43d42976d831be8008de199b657bab9499d4f487
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-04-25 20:16:16 +0500

    commit

---
+++ Lecture 19 Peak Index in Mountain Array  Binary Search  Leetcode 852/code.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+int main() {
+    vector<int> mountain = {0, 3, 8, 9, 5, 2};
+    cout << "Peak Value = " << peakMountain(mountain);
+    return 0;
+}
