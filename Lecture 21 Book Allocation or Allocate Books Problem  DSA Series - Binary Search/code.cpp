b88e74fc87a849588a10c6b584ebcd05398d16fe
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-04-27 05:56:08 +0500

    commit

---
+++ Lecture 21 Book Allocation or Allocate Books Problem  DSA Series - Binary Search/code.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+int main() {
+    vector<int> arr = {2, 1, 3, 4};
+    int n = 4;
+    int m = 2;
+
+    cout << allocateBooks(arr, n, m) << endl;
+    return 0;
+}
