f9b1216e94b248d9b8c934a0f10e365397fca13c
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-04-28 07:09:22 +0500

    commit

--- Lecture 21 Book Allocation or Allocate Books Problem  DSA Series - Binary Search/code.cpp
+++ Lecture 21 Book Allocation or Allocate Books Problem  DSA Series - Binary Search/code.cpp
@@ -2,50 +2,6 @@
 #include <vector>
 using namespace std;
 
-bool isValid(vector<int> &arr,int n,int m, int maxAllowedPages) { //O(n)
-    int stu = 1, pages = 0;
-
-    for(int i = 0 ; i < n ; i++) {
-        if(arr[i] > maxAllowedPages) {
-            return false;
-        }
-
-        if(pages + arr[i] <= maxAllowedPages) {
-            pages += arr[i];
-        } else {
-            stu++;
-            pages = arr[i];
-        }
-    }
-    return stu > m ? false : true;
-}
-
-
-int allocateBooks(vector<int> &arr, int n, int m) { //O(logN * n) Time Complexity
-    if(m > n) {
-        return -1;
-    }
-
-    int sum = 0;
-
-    for(int i = 0 ; i < n ; i++) { //O(n)
-        sum += arr[i];
-    }
-
-    int st = 0, end = sum;
-    int ans = -1;
-
-    while(st <= end) { 
-        int mid = st + (end-st)/2;
-
-        if(isValid(arr, n, m, mid)) {
-            ans = mid;
-            end = mid - 1;
-        } else{
-            st = mid + 1;
-        }
-    }
-}
 
 int main() {
     vector<int> arr = {2, 1, 3, 4};
