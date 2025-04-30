d478a53f50d4f35d3e1ba80693a9127a24e7c661
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-04-30 07:28:49 +0500

    commit

--- Lecture 24 Sorting Algorithms  Bubble Sort, Selection Sort & Insertion Sort/code.cpp
+++ Lecture 24 Sorting Algorithms  Bubble Sort, Selection Sort & Insertion Sort/code.cpp
@@ -19,6 +19,16 @@ using namespace std;
 //     }
 // }
 
+// selectionSort
+
+// void selectionSort(int arr[], int n) {
+//     for(int i = 0 ; i<n-1 ; i++) {
+//         int smallIndex = i;
+//         for(int j = i+1 ; j<n ; j++) {
+//             if(arr[j] < arr[smallIndex]){
+//                 smallIndex = j;
+//             }
+//         }
 
 
 int main()
