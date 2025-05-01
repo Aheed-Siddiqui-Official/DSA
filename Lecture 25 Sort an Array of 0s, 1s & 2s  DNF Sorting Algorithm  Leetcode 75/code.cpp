5b02040cd5fd6f8fea92a2d11ff9e2e590a0ee25
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-01 07:50:43 +0500

    commit

--- Lecture 25 Sort an Array of 0s, 1s & 2s  DNF Sorting Algorithm  Leetcode 75/code.cpp
+++ Lecture 25 Sort an Array of 0s, 1s & 2s  DNF Sorting Algorithm  Leetcode 75/code.cpp
@@ -6,6 +6,14 @@ using namespace std;
 int main() {
     vector<int> nums = {2, 0, 2, 1, 1, 0, 1, 2, 0, 0};
 
+                                        // BRUTE FORCE O(nlogn)
+
+    // sort(nums.begin(), nums.end());
+
+    // for(int i = 0 ; i<nums.size() ; i++) {
+    //     cout << nums[i];
+    // }
+
 
 
     return 0;
