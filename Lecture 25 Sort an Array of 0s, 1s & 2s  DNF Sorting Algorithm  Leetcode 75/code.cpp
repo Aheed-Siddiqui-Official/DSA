991b8878b7d5175a333ecdadc6b67357437a648e
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-01 07:52:23 +0500

    commit

--- Lecture 25 Sort an Array of 0s, 1s & 2s  DNF Sorting Algorithm  Leetcode 75/code.cpp
+++ Lecture 25 Sort an Array of 0s, 1s & 2s  DNF Sorting Algorithm  Leetcode 75/code.cpp
@@ -49,7 +49,15 @@ int main() {
     //     cout << x << " ";
     // }
 
+                                    // MOST OPTIMAL O(n) (DNF ALGORITHM)
 
+    int n = nums.size();
+    int low = 0, mid = 0, high = n-1;
 
+    while(mid <= high) {
+        if(nums[mid] == 0) {
+            swap(nums[low], nums[mid]);
+            mid++; low++;
+        } 
     return 0;
 }
