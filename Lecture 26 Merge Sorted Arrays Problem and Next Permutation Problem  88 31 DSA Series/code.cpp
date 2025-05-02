925e8ef60fa222d2f7716fa8e25aa398c581257e
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-02 08:06:19 +0500

    commit

---
+++ Lecture 26 Merge Sorted Arrays Problem and Next Permutation Problem  88 31 DSA Series/code.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+
+
+
+int main() {
+
+                                        // MERGE TWO SORTED ARRAY'S
+
+    int n = 3, m = 3;
+    vector <int> A = {1, 2, 3, 0, 0, 0};
+    vector <int> B = {4, 5, 6};
+    
+                                        // NEXT PERMUTATION PROBLEM
+
+    vector <int> A = {1, 2, 3};
+    nextPermutation(A);
+    return 0;
+}
