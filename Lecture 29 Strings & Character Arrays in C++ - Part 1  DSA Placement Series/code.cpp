3ceb69a9579e9fb7c3672b2ccc7915651e788c64
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-03 08:08:08 +0500

    commit

---
+++ Lecture 29 Strings & Character Arrays in C++ - Part 1  DSA Placement Series/code.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+                                            // CHARACTER & STRINGS(DYNAMIC)
+
+    // char str[] = {'a', 'b', 'c', '\0'}; // Until we dont end arr string with \0 it is not count as valid string. Null character is ignored in string length. 
+
+    // char str1[] = "hello";// String literals means which cant be changed. They are stored in from of array.
+
+    // cout << str[2] << endl;
+    // cout << str1;
+
+
+    return 0;
+}
