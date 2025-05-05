e49736368cee28536c04c5bf462fa4c48b6ced5f
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-05 21:58:32 +0500

    commit

---
+++ Lecture 31 Strings - Part 3  Permutation in String  DSA Placement Series/code.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+                                        // PERMUTATION IN STRING
+
+    string s1 = "bcdefabcdgef";
+    string s2 = "abc";
+    
+    cout << checkInclusion(s1, s2);
+    return 0;
+}
