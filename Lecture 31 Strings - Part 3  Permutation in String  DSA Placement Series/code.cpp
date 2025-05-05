6c5bdc8fae8633788e3077842f2f654517c1f8c7
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-05 21:59:21 +0500

    commit

--- Lecture 31 Strings - Part 3  Permutation in String  DSA Placement Series/code.cpp
+++ Lecture 31 Strings - Part 3  Permutation in String  DSA Placement Series/code.cpp
@@ -7,8 +7,17 @@ bool isFreqSame(int freq1[], int freq2[]) { // O(1)
             return false;
         }
     }
-    
+    return true;
 }
+
+bool checkInclusion(string s1, string s2) {
+    int freq [26] = {0};
+
+    for(int i=0 ; i<s1.length() ; i++) {
+        freq[s1[i] - 'a']++;
+    }
+
+    
 
 int main() {
                                         // PERMUTATION IN STRING
