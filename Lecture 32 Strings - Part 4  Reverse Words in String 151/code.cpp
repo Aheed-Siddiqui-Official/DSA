90d91417689d89ad6d97242e5c9ca6b5f04363e9
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-06 07:59:58 +0500

    commit

--- Lecture 32 Strings - Part 4  Reverse Words in String 151/code.cpp
+++ Lecture 32 Strings - Part 4  Reverse Words in String 151/code.cpp
@@ -11,6 +11,12 @@ string reverseWords(string s) {
     for(int i = 0 ; i<n ; i++) {
         string word = "";
 
+        while(i<n && s[i] != ' ') {
+            word += s[i];
+            i++;
+        }
+        reverse(word.begin(), word.end());
+
         
 }
 
