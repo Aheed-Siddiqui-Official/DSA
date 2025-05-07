0fc205edca4b591b0d651aa8a4daedc0cf2da0a1
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-07 07:07:29 +0500

    commit

--- Lecture 33 String Compression problem - Lecture 32  Leetcode 443/code.cpp
+++ Lecture 33 String Compression problem - Lecture 32  Leetcode 443/code.cpp
@@ -6,7 +6,17 @@ int compress(vector<char>& chars) {
     int n = chars.size();
     int idx = 0;
 
-    f
+    for(int i=0 ; i<n ; i++) {
+        char ch = chars[i];
+        int count = 0;
+
+        while(i<n && chars[i] == ch) {
+            count++;
+            i++;
+        }
+
+        
+}
 
 int main() {
     vector <char> chars = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
