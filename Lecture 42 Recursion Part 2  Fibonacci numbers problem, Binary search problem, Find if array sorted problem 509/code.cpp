3d8f8795d19e3ca140cf7abbf98f4510d4953544
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-09 19:00:27 +0500

    commit

--- Lecture 42 Recursion Part 2  Fibonacci numbers problem, Binary search problem, Find if array sorted problem 509/code.cpp
+++ Lecture 42 Recursion Part 2  Fibonacci numbers problem, Binary search problem, Find if array sorted problem 509/code.cpp
@@ -2,6 +2,15 @@
 #include <vector>
 using namespace std;
 
+                                        // NTH FIBONACCI TERM
+
+int fib(int n) {
+    if(n==0 || n==1){
+        return n;
+    }
+    return fib(n-1) + fib(n-2);
+}
+
 
 
 int main() {
