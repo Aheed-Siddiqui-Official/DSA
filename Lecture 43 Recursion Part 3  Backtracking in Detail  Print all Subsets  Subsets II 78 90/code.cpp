3a81ce43575aa61fa9ae8b6ada0c6d217300ea1a
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-10 18:34:01 +0500

    commit

--- Lecture 43 Recursion Part 3  Backtracking in Detail  Print all Subsets  Subsets II 78 90/code.cpp
+++ Lecture 43 Recursion Part 3  Backtracking in Detail  Print all Subsets  Subsets II 78 90/code.cpp
@@ -2,7 +2,18 @@
 #include <vector>
 using namespace std;
 
+void printSubsets(vector <int> &arr, vector <int> &ans, int i) {
+    if(i == arr.size()) {
+        for(int val:ans) {
+            cout<<val<<" ";
+        }
+        cout<<endl;
+        return;
+    }
 
+    //include
+    
+}
 
 int main() {
     vector <int>arr = {1, 2, 3};
