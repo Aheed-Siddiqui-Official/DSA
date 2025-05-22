6902aedaff74cb1d62200673c4a7ce68fcf15196
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-22 07:40:35 +0500

    commit

--- Lecture 56 OOPs/2) class.cpp
+++ Lecture 56 OOPs/2) class.cpp
@@ -17,4 +17,29 @@ public:
         dept = newDept;
     }
 
-    
+    //setter
+    void setSalary(double s) {
+        salary = s;
+    }
+
+    //getter
+    double getSalary() {
+        return salary;
+    }
+};
+
+
+
+int main() {
+    //object
+    Teacher t1;
+    t1.name = "MAS";
+    t1.dept = "CS";
+    t1.subject = "CS";
+    t1.setSalary(124.343);
+
+    cout << t1.name << endl;
+    cout << t1.getSalary() << endl;
+
+    return 0;
+}
