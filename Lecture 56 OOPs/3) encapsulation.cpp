b63b14cf25abe59e75aac100c6189d2d1422f9a0
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-23 08:03:45 +0500

    commit

--- Lecture 56 OOPs/3) encapsulation.cpp
+++ Lecture 56 OOPs/3) encapsulation.cpp
@@ -8,3 +8,20 @@ private:
     double balance;
     string password;
 
+public:
+    string accId;
+    string name;
+};
+
+
+
+int main() {
+    //object
+    Account a1;
+    a1.name = "MAS";
+    a1.accId = "123";
+
+    cout << a1.name << endl;
+
+    return 0;
+}
