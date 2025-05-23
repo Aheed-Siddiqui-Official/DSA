b9aacaafc598488499cb3daff5d5b42ac460b303
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-23 08:04:09 +0500

    commit

---
+++ Lecture 56 OOPs/4) constructor.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Teacher {
+private:
+    double salary;
+
+public:
+
+    //properties, attributes
+    string name;
+    string dept;
+    string subject;
+
+    //constructor creation
+    //non-parameterized constructor
+    // Teacher() {
+    //     // cout << "CS\n";
+    //     dept = "CS";
+    // }
+
+    //parameterized constructor
+    // Teacher (string n, string d, string s, double sal) {
+    //     name = n;
+    //     dept = d;
+    //     subject = s;
+    //     salary = sal;
+    // }
+
+
+    
