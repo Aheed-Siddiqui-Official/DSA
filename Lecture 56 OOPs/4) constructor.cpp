b8d7f01c3cdeda1ca7eeca27eaffa7f044a18d91
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-23 08:04:21 +0500

    commit

--- Lecture 56 OOPs/4) constructor.cpp
+++ Lecture 56 OOPs/4) constructor.cpp
@@ -29,4 +29,40 @@ public:
     // }
 
 
-    
+    //this function
+    Teacher (string name, string dept, string subject, double salary) {
+        this->name = name;
+        this->dept = dept;
+        this->subject = subject;
+        this->salary = salary;
+    }
+
+    //custom copy constructor
+    Teacher(Teacher &orgObj) {
+        cout << "iam copy cons...\n";
+        this->name = orgObj.name;
+        this->dept = orgObj.dept;
+        this->subject = orgObj.subject;
+        this->salary = orgObj.salary;
+    }
+
+
+    void getInfo() {
+        cout << "name : " << name << endl;
+        cout << "sub : " << subject << endl;
+    }
+};
+
+
+
+int main() {
+    //object
+    Teacher t1("MAS", "CS", "CPP", 123.21); //constructor call
+    t1.getInfo();
+
+    //copy constructor
+    Teacher t2(t1);
+    t2.getInfo();
+
+    return 0;
+}
