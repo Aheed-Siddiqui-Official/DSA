38a0a93fb34ed925ffdc4a03abb15c8a5aaf869e
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-27 07:24:46 +0500

    commit

--- Lecture 56 OOPs/9) abstraction.cpp
+++ Lecture 56 OOPs/9) abstraction.cpp
@@ -6,3 +6,16 @@ class Shape {
     virtual void draw() = 0;//pure virtual function, automatically abstract class, its only idea we can't make objects of this class
 };
 
+class Circle : public Shape {
+    public:
+    void draw() {
+        cout << "drawing a circle" << endl;
+    }
+};
+
+int main() {
+    //object of child class
+    Circle c1;
+    c1.draw();
+    return 0;
+}
