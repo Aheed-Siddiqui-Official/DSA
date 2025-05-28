7402d58d43e513c62ce21519ba0ef52c2565923f
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-28 19:15:24 +0500

    commit

---
+++ Lecture 57 Introduction to Linked List  Data Structures & Algorithms/pop_front.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+//creating linked list
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List {
+    Node* head;
+    Node* tail;
+
+public:
+    List() {
+        head = tail = NULL;
+    }
+
+    
