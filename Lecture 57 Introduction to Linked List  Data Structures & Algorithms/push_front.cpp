a3781d0b07a59e52f6d0c04b9b9eb9e3f8befb82
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-28 19:12:48 +0500

    commit

---
+++ Lecture 57 Introduction to Linked List  Data Structures & Algorithms/push_front.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+//creating linked list
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List {
+    Node* head;
+    Node* tail;
+
+public:
+    List() {
+        head = tail = NULL;
+    }
