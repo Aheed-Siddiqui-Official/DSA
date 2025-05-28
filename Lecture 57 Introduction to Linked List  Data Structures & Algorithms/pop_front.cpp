ebecb1ad112a1137f516706f0357bc43c3acae35
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-28 19:15:54 +0500

    commit

--- Lecture 57 Introduction to Linked List  Data Structures & Algorithms/pop_front.cpp
+++ Lecture 57 Introduction to Linked List  Data Structures & Algorithms/pop_front.cpp
@@ -53,4 +53,27 @@ public:
         
     }
 
-   
+    //Print a ll
+    void printLL() {
+        Node* temp = head;
+
+        while(temp != NULL) {
+            cout << temp->data << "->";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+int main() {
+    List ll;
+
+    ll.push_front(1);
+    ll.push_front(2);
+    ll.push_front(3);
+
+    ll.pop_front();
+
+    ll.printLL();
+    return 0;
+}
