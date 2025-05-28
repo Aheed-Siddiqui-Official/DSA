ccfaf5e4c9c6537a295e72a097250797c5b05bc8
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-28 19:13:40 +0500

    commit

--- Lecture 57 Introduction to Linked List  Data Structures & Algorithms/push_front.cpp
+++ Lecture 57 Introduction to Linked List  Data Structures & Algorithms/push_front.cpp
@@ -21,3 +21,31 @@ public:
     List() {
         head = tail = NULL;
     }
+
+    //Push front logic
+    void push_front(int val) {
+        Node* newNode = new Node(val);//dynamic when used new key word and also process in memory after function call
+        
+        //When no node exists
+        if(head == NULL) {
+            head = tail = newNode;
+            return;
+        }
+        else {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    //Print a ll
+    void printLL() {
+        Node* temp = head;
+
+        while(temp != NULL) {
+            cout << temp->data << "->";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
