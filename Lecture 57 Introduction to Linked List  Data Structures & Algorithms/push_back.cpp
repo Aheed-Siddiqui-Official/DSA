e54c706008fd15359a4c6bd851575ef08ff6c2ed
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-29 11:42:12 +0500

    commit

--- Lecture 57 Introduction to Linked List  Data Structures & Algorithms/push_back.cpp
+++ Lecture 57 Introduction to Linked List  Data Structures & Algorithms/push_back.cpp
@@ -13,3 +13,27 @@ public:
     }
 };
 
+class List {
+    Node* head;
+    Node* tail;
+
+public:
+    List() {
+        head = tail = NULL;
+    }
+
+    //Push back logic
+    void push_back(int val) {
+        Node* newNode = new Node(val);
+
+        if(head == NULL) {
+            head = tail = newNode;
+            return;
+        }
+        else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    
