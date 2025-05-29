7513ff4d75a30410edd8695612a4754b85c6ea6f
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-29 11:43:06 +0500

    commit

--- Lecture 57 Introduction to Linked List  Data Structures & Algorithms/pop_back.cpp
+++ Lecture 57 Introduction to Linked List  Data Structures & Algorithms/pop_back.cpp
@@ -22,4 +22,36 @@ public:
         head = tail = NULL;
     }
 
+    //Push front logic
+    void push_front(int val) {
+        Node* newNode = new Node(val);//dynamic when used new key word and also process in memory after function call
+        
+        //When no node exists
+        if(head == NULL) {
+            head = tail = newNode;
+            return;
+        }
+        else {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    //Pop back logic
+    void pop_back() {
+        if(head == NULL) {
+            cout << "LL is empty\n";
+            return;
+        }
+        else {
+            Node* temp = head;
+            while(temp->next != tail) {
+                temp = temp->next;
+            }
+            temp->next = NULL;
+            delete tail;//tail data delete's in heap storage
+            tail = temp;//assign tail variable value to temp variable value
+        }
+    }
+
     
