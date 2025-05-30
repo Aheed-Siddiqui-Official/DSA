7af025b71e3444a3fecb7aec67ba1382f37e7499
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-30 19:25:47 +0500

    commit

--- Lecture 57 Introduction to Linked List  Data Structures & Algorithms/insert_mid.cpp
+++ Lecture 57 Introduction to Linked List  Data Structures & Algorithms/insert_mid.cpp
@@ -13,3 +13,56 @@ public:
     }
 };
 
+class List {
+    Node* head;
+    Node* tail;
+
+public:
+    List() {
+        head = tail = NULL;
+    }
+
+    //Push front logic
+    void push_front(int val) {
+        Node* newNode = new Node(val);//dynamic when used new key word and also process in memory after function call
+        
+        //When no node exists
+        if(head == NULL) {
+            head = tail = newNode;
+            return;
+        }
+        else {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    //Insert Middle Logic
+    void insert(int val, int pos) {
+        if(pos < 0) {
+            cout << "invalid pos\n";
+            return;
+        }
+
+        else if(pos == 0) {
+            push_front(val);
+            return;
+        }
+
+        else{
+            Node* temp = head;
+            for(int i = 0 ; i<pos-1 ; i++) {
+                if(temp == NULL) {
+                    cout << "invalid pos\n";
+                    return;
+                }
+                temp = temp->next;
+            }
+
+            Node* newNode = new Node(val);
+            newNode->next = temp->next;
+            temp->next = newNode;
+        }
+    }
+
+    
