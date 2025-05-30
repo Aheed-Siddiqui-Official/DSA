04093f21c7ef0d4e450cc3ef6faabc0598af637f
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-30 19:26:32 +0500

    commit

--- Lecture 57 Introduction to Linked List  Data Structures & Algorithms/search.cpp
+++ Lecture 57 Introduction to Linked List  Data Structures & Algorithms/search.cpp
@@ -13,3 +13,43 @@ public:
     }
 };
 
+class List {
+    Node* head;
+    Node* tail;
+
+public:
+    List() {
+        head = tail = NULL;
+    }
+
+    //Push front logic
+    void push_front(int val) {
+        Node* newNode = new Node(val);//dynamic when used new key word and also process in memory after function call
+        
+        //When no node exists
+        if(head == NULL) {
+            head = tail = newNode;
+            return;
+        }
+        else {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    //Search in a LL
+    int search(int key) {
+        Node* temp = head;
+        int idx = 0;
+
+        while(temp != NULL) {
+            if(temp->data == key) {
+                return idx;
+            }
+            temp = temp->next;
+            idx++;
+        }
+        return -1;
+    }
+
+    
