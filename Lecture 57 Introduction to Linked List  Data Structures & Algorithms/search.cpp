c5254eba4cd965a1ac11b2645fbe629716c8bb52
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-30 19:26:44 +0500

    commit

--- Lecture 57 Introduction to Linked List  Data Structures & Algorithms/search.cpp
+++ Lecture 57 Introduction to Linked List  Data Structures & Algorithms/search.cpp
@@ -52,4 +52,27 @@ public:
         return -1;
     }
 
-    
+    //Print a ll
+    void printLL() {
+        Node* temp = head;
+
+        while(temp != NULL) {
+            cout << temp->data << "->";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+int main() {
+    List ll;
+
+    ll.push_front(1);
+    ll.push_front(2);
+    ll.push_front(3);
+
+    ll.printLL();
+
+    cout << "Pos : " << ll.search(3) << endl;;
+    return 0;
+}
