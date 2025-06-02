956b13462e6758ea5bb0705920e510f48bd96489
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-06-02 08:02:18 +0500

    commit

--- Lecture 60 Detect & Remove Cycle in Linked List 141 142/code.cpp
+++ Lecture 60 Detect & Remove Cycle in Linked List 141 142/code.cpp
@@ -62,3 +62,24 @@ public:
 
                                     // Program to remove the cycle
 
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        bool isCycle = false;
+
+        while(fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast) {
+                isCycle = true;
+                break;
+            }
+        }
+        if(!isCycle) {
+            return NULL;
+        }
+
+        
