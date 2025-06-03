2c612ccba81e542c11cf355a745667c0570cc504
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-06-03 20:07:47 +0500

    commit

--- Lecture 63 Doubly LL/pop_back.cpp
+++ Lecture 63 Doubly LL/pop_back.cpp
@@ -13,3 +13,41 @@ public:
     }
 };      
 
+class DoublyList {
+    Node* head;
+    Node* tail;
+
+public:
+    DoublyList() {
+        head = tail = NULL;
+    }
+
+    void push_front(int val) {
+        Node* newNode = new Node(val);
+
+        if(head == NULL) {
+            head = tail = newNode;
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
+
+    void pop_back() {
+        if(head == NULL) {
+            cout << "DLL is empty" << endl;
+            return;
+        }
+
+        Node* temp = tail;
+        tail = tail->prev;
+
+        if(tail != NULL) {
+            tail->next = NULL;
+        }
+
+        temp->prev = NULL;
+    }
+
+    
