9fdf31f0dd82a5fbf3a38314212903754305302d
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-06-03 20:07:10 +0500

    commit

--- Lecture 63 Doubly LL/main.cpp
+++ Lecture 63 Doubly LL/main.cpp
@@ -13,3 +13,17 @@ public:
     }
 };      
 
+class DoublyList {
+    Node* head;
+    Node* tail;
+
+public:
+    DoublyList() {
+        head = tail = NULL;
+    }
+};
+
+int main() {
+    DoublyList dll;
+    return 0;
+}
