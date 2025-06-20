8eb965dca2e3aa86777723bac28aca4a40bf30a7
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-06-20 19:14:57 +0500

    commit

--- Lecture 63 Doubly LL/push_back.cpp
+++ Lecture 63 Doubly LL/push_back.cpp
@@ -13,3 +13,37 @@ public:
     }
 };      
 
+class DoublyList {
+    Node* head;
+    Node* tail;
+
+public:
+    DoublyList() {
+        head = tail = NULL;
+    }
+
+    void push_front(int val) {
+        Node* newNode = new Node(val);
+
+        if(head == NULL) {
+            head = tail = newNode;
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
+
+    
+    void push_back(int val) {
+        Node* newNode = new Node(val);
+
+        if(head == NULL) {
+            head = tail = newNode;
+        } else {
+            newNode->prev = tail;
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
