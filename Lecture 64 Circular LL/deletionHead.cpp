936fe43f0e890ba96f03814f6f2df1f41c6f8cb0
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-06-23 08:22:29 +0500

    commit

---
+++ Lecture 64 Circular LL/deletionHead.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+class CircularList {
+    Node* head;
+    Node* tail;
+
+public:
+    CircularList() {
+        head = tail = NULL;
+    }
+
+    void insertNode(int val) {
+        Node* newNode = new Node(val);
+
+        if(tail == NULL) {
+            head = tail = newNode;
+            tail->next = head;
+        } else {
+            newNode-> next = head;
+            head = newNode;
+            tail->next = head;
+        }
+    }
+
+    void deleteNode() {
+        if(head == NULL) {
+            return;
+        } else if(head == tail) {
+            delete head;
+            head = tail = NULL;
+        } else {
+            Node* temp = head;
+            head = head->next;
+            tail->next = head;
+
+            temp->next = NULL;
+            delete temp;
+        }
+    }
+
+    void print() {
+        if(head == NULL) return;
+
+        cout << head->data << "->";
+
+        Node*temp = head->next;
+        while(temp != head) { // written head instead of NULL to avoid infinite loop
+            cout << temp->data << "->";
+            temp = temp->next;
+        }
+
+        cout << temp->data << endl;
+    }
+};
+
+int main() {
+    CircularList cll;
+
+    cll.insertNode(1);
+    cll.insertNode(2);
+    cll.insertNode(3);
+
+    cll.print();
+
+    cll.deleteNode();
+
+    cll.print();
+    return 0;
+}
