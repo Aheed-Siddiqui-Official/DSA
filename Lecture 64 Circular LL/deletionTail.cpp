807c8963fe26ed872307d193e02d10beea50d4c2
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-06-24 07:26:34 +0500

    commit

--- Lecture 64 Circular LL/deletionTail.cpp
+++ Lecture 64 Circular LL/deletionTail.cpp
@@ -12,3 +12,46 @@ public:
     }
 };
 
+class CircularList {
+    Node* head;
+    Node* tail;
+
+public:
+    CircularList() {
+        head = tail = NULL;
+    }
+
+    void insertNode(int val) {
+        Node* newNode = new Node(val);
+
+        if(tail == NULL) {
+            head = tail = newNode;
+            tail->next = head;
+        } else {
+            newNode-> next = head;
+            head = newNode;
+            tail->next = head;
+        }
+    }
+
+    void deleteNode() {
+        if(head == NULL) {
+            return;
+        } else if(head == tail) {
+            delete head;
+            head = tail = NULL;
+        } else {
+            Node* temp = tail;
+            Node* prev = head;
+
+            while(prev->next != tail) {
+                prev = prev->next;
+            }
+            tail = prev;
+            tail->next = head;
+
+            temp->next = NULL;
+            delete temp;
+        }
+    }
+
