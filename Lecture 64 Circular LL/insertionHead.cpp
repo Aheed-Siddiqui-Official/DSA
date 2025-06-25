263a6b26dba36750b9758d9079fb1a31ab38c84d
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-06-25 20:01:23 +0500

    commit

--- Lecture 64 Circular LL/insertionHead.cpp
+++ Lecture 64 Circular LL/insertionHead.cpp
@@ -12,3 +12,26 @@ public:
     }
 };
 
+class CircularList {
+    Node* head;
+    Node* tail;
+
+public:
+    CircularList() {
+        head = tail = NULL;
+    }
+
+    void insertNode(int val) {
+        Node* newNode = new Node(val);
+
+        if(tail == NULL) {
+            head = tail = newNode;
+            tail->next = head;
+        } else {
+            newNode-> next = head;
+            head = newNode;
+            tail->next = head;
+        }
+    }
+
+    
