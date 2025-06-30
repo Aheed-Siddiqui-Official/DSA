7926a68dec4fcea7c5fad24f9e224a1cc7ef0ff9
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-06-30 19:23:19 +0500

    commit

--- Lecture 68 Stacks/code.cpp
+++ Lecture 68 Stacks/code.cpp
@@ -27,3 +27,60 @@ public:
     }
 };
 
+                                        // STACK FROM SCRATCH USING LL
+
+class StackLL {
+    list<int> ll;
+
+public:
+    void push(int val) { //O(1)
+        ll.push_front(val);
+    }
+
+    void pop() { //O(1)
+        ll.pop_front();
+    }
+
+    int top() { //O(1)
+        return ll.front();       
+    }
+
+    bool empty() {
+        return ll.size() == 0;
+    }
+};
+
+int main() {
+
+                                        // STACK FROM SCRATCH USING A VECTOR
+
+    // StackVec s;
+    // s.push(1);
+    // s.push(2);
+    // s.push(3);
+
+                                        // STACK FROM SCRATCH USING LL
+
+    // StackLL ll;
+    // ll.push(1);
+    // ll.push(2);
+    // ll.push(3);
+
+                                        // STACK FROM STL
+
+    stack<int> s;
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    
+
+    while(!s.empty()) {
+        cout << s.top() << " ";
+        s.pop();
+    }
+
+    cout << endl;
+
+    return 0;
+}
