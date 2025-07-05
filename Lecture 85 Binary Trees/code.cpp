95ce8e111c0df4c9ef6ff211dec6f2db14516afb
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-07-05 19:30:28 +0500

    commit

--- Lecture 85 Binary Trees/code.cpp
+++ Lecture 85 Binary Trees/code.cpp
@@ -13,3 +13,19 @@ public:
         left = right = NULL;
     }
     
+};
+
+static int idx = -1;
+Node* builtTree(vector<int> preOrder) {
+    idx++;
+
+    if(preOrder[idx] == -1) {
+        return NULL;
+    }
+
+    Node* root = new Node(preOrder[idx]);
+    root->left = builtTree(preOrder); //LEFT SUBTREE
+    root->right = builtTree(preOrder); //RIGHT SUBTREE
+    return root;
+}
+
