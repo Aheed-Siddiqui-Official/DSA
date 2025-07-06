e60ac589b3b514e04143b4d95fe04a7532caa112
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-07-06 18:58:59 +0500

    commit

--- Lecture 85 Binary Trees/inOrder.cpp
+++ Lecture 85 Binary Trees/inOrder.cpp
@@ -15,3 +15,28 @@ public:
     
 };
 
+static int idx = -1;
+Node* builtTree(vector<int> preOrder) {
+    idx++;
+
+    if(preOrder[idx] == -1) {
+        return NULL;
+    }
+
+    Node* root = new Node(preOrder[idx]);
+    root->left = builtTree(preOrder); //LEFT SUBTREE
+    root->right = builtTree(preOrder); //RIGHT SUBTREE
+    return root;
+}
+
+//inorder
+void inOrder(Node* root) {
+    if(root == NULL){
+        return;
+    } else {        
+        inOrder(root->left);
+        cout << root->data << " ";
+        inOrder(root->right);
+    }
+}
+
