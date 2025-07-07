0293100268f702eabfaacaeeefea84d6a1f596dd
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-07-07 19:58:31 +0500

    commit

--- Lecture 85 Binary Trees/levelOrder.cpp
+++ Lecture 85 Binary Trees/levelOrder.cpp
@@ -16,3 +16,51 @@ public:
     
 };
 
+static int idx = -1;
+Node* builtTree(vector<int> preOrder) {
+    idx++;
+
+    if(preOrder[idx] == -1) {
+        return NULL;
+    }
+
+    Node* root = new Node(preOrder[idx]);
+    root->left = builtTree(preOrder); //LEFT SUBTREE
+    root->right = builtTree(preOrder); //RIGHT SUBTREE
+    return root;
+}
+
+//postorder
+void levelOrder(Node* root) {
+    queue<Node*> q;
+
+    q.push(root);
+    q.push(NULL);
+
+    while (q.size() > 0) {
+        Node* curr = q.front();
+        q.pop();
+
+        if(curr == NULL) {
+            if(!q.empty()) {
+                cout << endl;
+                q.push(NULL);
+                continue;
+            } else {
+                break;
+            }
+        }
+
+         cout << curr->data << " ";
+
+        if(curr->left != NULL) {
+            q.push(curr->left);
+        }
+
+        if(curr->right != NULL) {
+            q.push(curr->right);
+        }
+    }
+    cout << endl;
+}
+
