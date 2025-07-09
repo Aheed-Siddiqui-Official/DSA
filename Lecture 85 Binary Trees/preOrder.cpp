e426c87ed175a1f2997540e246accfd693f2eca5
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-07-09 20:28:43 +0500

    commit

--- Lecture 85 Binary Trees/preOrder.cpp
+++ Lecture 85 Binary Trees/preOrder.cpp
@@ -40,3 +40,12 @@ void preOrder(Node* root) {
     }
 }
 
+int main() {
+    vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+    
+    Node* root = builtTree(preorder);
+    
+    preOrder(root);
+    cout << endl;
+    return 0;
+}
