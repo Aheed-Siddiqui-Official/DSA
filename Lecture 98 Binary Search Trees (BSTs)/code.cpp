7aa5d5842cedc5e2ae7ff76a31570f827a7ef5ef
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-07-11 19:12:14 +0500

    commit

--- Lecture 98 Binary Search Trees (BSTs)/code.cpp
+++ Lecture 98 Binary Search Trees (BSTs)/code.cpp
@@ -14,3 +14,25 @@ public:
     }
 };
 
+Node* insert(Node* root, int val) {
+    if(root == NULL) {
+        return new Node(val);
+    }
+
+    if(val < root->data) {
+        root->left = insert(root->left, val);
+    } else {
+        root->right = insert(root->right, val);
+    }
+    return root;
+}
+
+Node* buildBST(vector<int> arr) {
+    Node* root = NULL;
+
+    for(int val : arr) {
+        root = insert(root, val);
+    }
+    return root;
+}
+
