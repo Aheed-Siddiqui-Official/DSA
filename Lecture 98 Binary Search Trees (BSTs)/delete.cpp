b8a430a8e384eb0dc7b32c1d83f08e440f01b9cc
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-07-12 19:22:32 +0500

    commit

--- Lecture 98 Binary Search Trees (BSTs)/delete.cpp
+++ Lecture 98 Binary Search Trees (BSTs)/delete.cpp
@@ -14,3 +14,56 @@ public:
     }
 };
 
+Node* insert(Node* root, int val) {
+    if(root == NULL) {
+        return new Node(val);
+    }
+
+    if(val < root->data) {
+        root->left = insert(root->left, val);
+    } else {
+        root->right = insert(root->right, val);
+    }
+    return root;
+}
+
+Node* buildBST(vector<int> arr) {
+    Node* root = NULL;
+
+    for(int val : arr) {
+        root = insert(root, val);
+    }
+    return root;
+}
+
+void inOrder(Node* root){
+    if(root == NULL) {
+        return;
+    }
+
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+bool search(Node* root, int key) {
+    if(root == NULL) {
+        return false;
+    }
+    if(root->data == key) {
+        return true;
+    }
+    if(key < root->data) {
+        return search(root->left, key);
+    } else {
+        return search(root->right, key);
+    }
+}
+
+Node* getInorderSuccessor(Node* root) { // left most node in right subtree
+    while(root != NULL && root->left != NULL) {
+        root = root->left;
+    }
+    return root;
+}
+
